a0d4c3229d6c30689240aade0b44a7a5a9859590
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-05-24 16:17:56 +0000

    User Authentication

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,24 +1,21 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+  current_user.places.create(place_params)
+  redirect_to root_path
 end
 
-def new
-   @place = Place.new
-  end
+private
 
- def create
-  end
-  def create
-    Place.create(place_params)
-    redirect_to root_path
-  end
-
-  private
-
-  def place_params
-    params.require(:place).permit(:name, :description, :address)
-  end
-  end
-
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+end
