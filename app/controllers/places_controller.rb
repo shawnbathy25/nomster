dc4efc0bcda3084a375e8be619fdfbb5cb013176
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-05-25 13:55:14 +0000

    Edit update and delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,9 +14,13 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
-  params.require(:place).permit(:name, :description, :address)
+    params.require(:place).permit(:name, :description, :address)
   end
-  end
+end
