5cd170206ffade6f6feb7e5801039de1e06d778f
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-06-03 18:45:35 +0000

    adding an image

--- Gemfile
+++ Gemfile
@@ -81,3 +81,7 @@ gem 'devise'
 gem "geocoder"
 
 gem "figaro"
+
+gem 'carrierwave', '~> 1.0'
+
+gem "fog-aws"
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,7 @@ end
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
 
   def edit
